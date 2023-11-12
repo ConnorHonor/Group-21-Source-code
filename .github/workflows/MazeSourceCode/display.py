b151727b86ef36cee5025635fb131cc3c8588230
conthom <conthom@iu.edu>
2023-11-12 16:25:47 -0500

    This is the folder our code is in, documenting

--- .github/workflows/MazeSourceCode/display.py
+++ .github/workflows/MazeSourceCode/display.py
@@ -1,3 +1,6 @@
+# This code is from the mazelib library by John Stilley
+# May change this to also show PNG of the solution squares and start / end square
+# Currently just shows the maze with black and white squares
 import matplotlib.pyplot as plt
 
 def showPNG(grid):
--- .github/workflows/MazeSourceCode/mazegen.py
+++ .github/workflows/MazeSourceCode/mazegen.py
@@ -1,6 +1,9 @@
 # Group 21, Maze Solving AI Source Code
 # Connor Thompson, John Huddleston, Aidan Blashe
 # Imports
+# This code is supposed to pick from the maze generators at random, solve them using specified algorithm, and save the length of the solutions 
+# and possibly the runtime it took to get the solution. 
+# Testing with Sidewinder maze building alg and Tremaux solving alg
 from display import showPNG
 from mazelib.mazelib import Maze
 from mazelib.generate.Sidewinder import Sidewinder
@@ -10,4 +13,7 @@ m.generator = Sidewinder(24,33)
 # m.generate_entrances()
 m.generate()
 m.solver = Tremaux()
+m.generate_entrances()
+m.solve()
+print(m)
 showPNG(m.grid)

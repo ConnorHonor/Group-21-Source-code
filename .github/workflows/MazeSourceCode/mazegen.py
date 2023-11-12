ef99180d24e71331e3e3f4302d95d5d3fff929dd
conthom <conthom@iu.edu>
2023-11-12 16:35:00 -0500

    This is the main file for our source code

--- .github/workflows/MazeSourceCode/mazegen.py
+++ .github/workflows/MazeSourceCode/mazegen.py
@@ -1,15 +1,16 @@
 # Group 21, Maze Solving AI Source Code
 # Connor Thompson, John Huddleston, Aidan Blashe
 # Imports
-# This code is supposed to pick from the maze generators at random, solve them using specified algorithm, and save the length of the solutions 
-# and possibly the runtime it took to get the solution. 
-# Testing with Sidewinder maze building alg and Tremaux solving alg
-######################################################################
-# To install mazelib enter "pip install mazelib" into terminal
 from display import showPNG
 from mazelib.mazelib import Maze
 from mazelib.generate.Sidewinder import Sidewinder
 from mazelib.solve.Tremaux import Tremaux
+# This code is supposed to pick from the maze generators at random, solve them using specified algorithm, and save the length of the solutions 
+# and possibly the runtime it took to get the solution. 
+# Testing with Sidewinder maze building alg and Tremaux solving alg
+######################################################################
+# To install mazelib enter "pip install mazelib" into terminal.
+
 m = Maze()
 m.generator = Sidewinder(24,33)
 # m.generate_entrances()

82689143f35c9903767a7b98096333dc83ce8d7e
conthom <conthom@iu.edu>
2023-11-26 11:32:03 -0600

    Adding imports of all algorithms

--- .github/workflows/MazeSourceCode/mazegen.py
+++ .github/workflows/MazeSourceCode/mazegen.py
@@ -1,22 +1,24 @@
 # Group 21, Maze Solving AI Source Code
 # Connor Thompson, John Huddleston, Aidan Blashe
-# Imports
 from display import showPNG
 from mazelib.mazelib import Maze
-from mazelib.generate.Sidewinder import Sidewinder
-from mazelib.solve.Tremaux import Tremaux
-# This code is supposed to pick from the maze generators at random, solve them using specified algorithm, and save the length of the solutions 
+from mazelib.generate import AldousBroder, BacktrackingGenerator, BinaryTree, CellularAutomaton, Division, DungeonRooms, Ellers, GrowingTree, HuntAndKill, Kruskal, Prims, Sidewinder, TrivialMaze
+from mazelib.solve import BacktrackingSolver, Chain, Collision, Dijkstra, MazeSolveAlgo, RandomMouse, ShortestPath, ShortestPaths, Tremaux
+
+# This code is supposed to pick from the maze generators at random, solve them using specified algorithm, and save the length of the solutions
 # and possibly the runtime it took to get the solution. 
 # Testing with Sidewinder maze building alg and Tremaux solving alg
 ######################################################################
 # To install mazelib enter "pip install mazelib" into terminal.
 
 m = Maze()
-m.generator = Sidewinder(24,33)
+m.generator = Sidewinder(15,15)
 # m.generate_entrances()
 m.generate()
 m.solver = Tremaux()
 m.generate_entrances()
 m.solve()
 print(m)
-showPNG(m.grid)
+# showPNG(m.grid)
+solution = m.solutions
+print(len(solution[0])) # Get length of solution for current solve
